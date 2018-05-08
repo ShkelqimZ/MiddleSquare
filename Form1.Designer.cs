6887ed0d480dcfc61d2db012a87da47f1d760352
PC <shkelqim.zahiti@gmail.com>
2018-05-08 19:10:36 +0200

    Middle square method

---
+++ Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Middle_Square_Method
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnClean = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.nmrNum = new System.Windows.Forms.NumericUpDown();
+            this.txtSeed = new System.Windows.Forms.TextBox();
+            this.nmrDigits = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nmrNum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nmrDigits)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.groupBox1.Controls.Add(this.btnClean);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.nmrNum);
+            this.groupBox1.Controls.Add(this.txtSeed);
+            this.groupBox1.Controls.Add(this.nmrDigits);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(531, 248);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // btnClean
+            // 
+            this.btnClean.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClean.Location = new System.Drawing.Point(427, 157);
+            this.btnClean.Name = "btnClean";
+            this.btnClean.Size = new System.Drawing.Size(84, 32);
+            this.btnClean.TabIndex = 7;
+            this.btnClean.Text = "Clean";
+            this.btnClean.UseVisualStyleBackColor = true;
+            this.btnClean.Click += new System.EventHandler(this.btnClean_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(309, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 32);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // nmrNum
+            // 
+            this.nmrNum.Location = new System.Drawing.Point(309, 117);
+            this.nmrNum.Name = "nmrNum";
+            this.nmrNum.Size = new System.Drawing.Size(202, 23);
+            this.nmrNum.TabIndex = 5;
+            // 
+            // txtSeed
+            // 
+            this.txtSeed.Location = new System.Drawing.Point(309, 88);
+            this.txtSeed.Name = "txtSeed";
+            this.txtSeed.Size = new System.Drawing.Size(202, 23);
+            this.txtSeed.TabIndex = 4;
+            // 
+            // nmrDigits
+            // 
+            this.nmrDigits.Location = new System.Drawing.Point(309, 59);
+            this.nmrDigits.Name = "nmrDigits";
+            this.nmrDigits.Size = new System.Drawing.Size(202, 23);
+            this.nmrDigits.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(244, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Number of pseudorandom numbers";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Seed";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of digits";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.groupBox2.Controls.Add(this.rtbOutput);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 279);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(531, 148);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Output";
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbOutput.Location = new System.Drawing.Point(5, 32);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.ReadOnly = true;
+            this.rtbOutput.Size = new System.Drawing.Size(506, 110);
+            this.rtbOutput.TabIndex = 0;
+            this.rtbOutput.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(555, 439);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Middle Square Method";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nmrNum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nmrDigits)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown nmrNum;
+        private System.Windows.Forms.TextBox txtSeed;
+        private System.Windows.Forms.NumericUpDown nmrDigits;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnClean;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Middle_Square_Method
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        
+        public string algoritmi(double seed)
+        {
+            double[] randomNumbers = new double[Convert.ToInt32(nmrNum.Value)];
+            double var1 = double.Parse(txtSeed.Text);
+            string show = "";
+            for (int i = 0; i < nmrNum.Value;i++ )
+            {
+               
+                string squareSeedz = "";
+                string seedS = "";
+                double squareSeed ;
+                seedS = seed.ToString();
+                
+                squareSeed = double.Parse(seedS) * double.Parse(seedS);
+                squareSeedz = squareSeed.ToString();
+                string squareSeedzz = "";
+                
+                
+                if (squareSeed.ToString().Length % 2 == 1 && seed.ToString().Length % 2 == 0)
+                {
+                    squareSeedz = "0" + squareSeedz;
+                }
+                else if (squareSeed.ToString().Length % 2 == 0 && seed.ToString().Length % 2 == 1 && seed.ToString().Length * 2 != squareSeed.ToString().Length)
+                {
+                    
+                     for (int k = 0; k < ((var1 * var1).ToString().Length - squareSeedz.Length); k++)
+                     {
+                         squareSeedzz += "0";
+                     }
+                     squareSeedz = squareSeedzz + squareSeed;
+                    
+                }
+                else if (squareSeed.ToString().Length % 2 == 0 && seed.ToString().Length % 2 == 1 && seed.ToString().Length<3)
+                {
+                    for (int k = 0; k < ((var1 * var1).ToString().Length - squareSeedz.Length); k++)
+                     {
+                         squareSeedzz += "0";
+                     }
+                     squareSeedz = squareSeedzz + squareSeed;
+                }
+
+                double middleD = 0;
+                
+                if (squareSeed.ToString().Length == 1 && seed.ToString().Length == 1)
+                {
+                    middleD = 0;
+                }
+                else
+                {
+                    
+                    int NR = Convert.ToInt32(nmrDigits.Value) / 2;
+                    int start = (squareSeedz.Length / 2) - NR;
+                    int length = Convert.ToInt32(nmrDigits.Value);
+                    
+                    string middleNum = squareSeedz.Substring(start, length);
+                    middleD = double.Parse(middleNum);
+                }
+                
+                randomNumbers[i] += middleD;
+                
+                seed = middleD;
+
+                if (seed.ToString().Length != nmrDigits.Value)
+                {
+                    for (int u = 0; u < nmrDigits.Value - seed.ToString().Length; u++)
+                    {
+                        show += "0";
+                    }
+                    show +=  randomNumbers[i] +" ";
+                }
+                else
+                {
+                    show += randomNumbers[i].ToString() + " ";
+                }
+            }
+
+            return show;
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (nmrDigits.Value==0)
+            {
+                MessageBox.Show("Please input the number of digits !","WARNING",MessageBoxButtons.OK);
+            }
+            else if (txtSeed.Text == "")
+            {
+                MessageBox.Show("Please input the seed number");
+                txtSeed.Focus();
+            }
+            else if (nmrNum.Value==0)
+            {
+                MessageBox.Show("Please give the number of pseudorandom numbers you want !", "WARNING", MessageBoxButtons.OK);
+            }
+            else if (nmrDigits.Value%2==1)
+            {
+                MessageBox.Show("Accuracy for odd numbers are weak.\n Please input even number of digits !", "WARNING", MessageBoxButtons.OK);
+                
+                nmrDigits.Focus();
+            }
+            else if (txtSeed.Text.Length != nmrDigits.Value)
+            {
+                MessageBox.Show("Please, number of  seed's digits and number of digits you input are not equal !");
+            }
+            else
+            {
+
+                rtbOutput.Text += algoritmi(double.Parse(txtSeed.Text));
+            }
+            
+        }
+
+        private void btnClean_Click(object sender, EventArgs e)
+        {
+            rtbOutput.Clear();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+       
+       
+    }
+    
+}
